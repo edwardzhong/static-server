c9448fffa6e2f1a359ba00409b592404e5daf17b
edwardzhong <zhongjfgd@163.com>
2020-02-04 01:26:43 +0800

    tab

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const koa = require('koa')
-const app = new koa()
-const static = require('koa-static')
-const favicon = require('koa-favicon')
-const path = require('path')
+const koa = require('koa');
+const app = new koa();
+const static = require('koa-static');
+const favicon = require('koa-favicon');
+const path = require('path');
 const port = 3001;
 
 // set static directiory
-app.use(static(path.join(__dirname,'dist')));
+app.use(static(path.join(__dirname, 'dist')));
 app.use(favicon(path.join(__dirname, 'favicon.jpg')));
 
 //if historyApiFallback mode
@@ -22,19 +22,20 @@ app.use(favicon(path.join(__dirname, 'favicon.jpg')));
 
 // deal 404
 app.use(async (ctx, next) => {
-    ctx.status = 404;
-    ctx.body = '404! page not found !';
+  ctx.status = 404;
+  ctx.body = '404! page not found !';
 });
 
 // koa already had middleware to deal with the error, just rigister the error event
 app.on('error', (err, ctx) => {
-    ctx.status = 500;
-    ctx.statusText = 'Internal Server Error';
-    if (ctx.app.env === 'development') { //throw the error to frontEnd when in the develop mode
-        ctx.res.end(err.stack); //finish the response
-    } else {
-        ctx.render('Server Error');
-    }
+  ctx.status = 500;
+  ctx.statusText = 'Internal Server Error';
+  if (ctx.app.env === 'development') {
+    //throw the error to frontEnd when in the develop mode
+    ctx.res.end(err.stack); //finish the response
+  } else {
+    ctx.render('Server Error');
+  }
 });
 
 app.listen(port);
